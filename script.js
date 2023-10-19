759f41c712be630ec28ea527a56650b70d153f87
LemonGod66 <147831840+LemonGod66@users.noreply.github.com>
2023-10-19 09:30:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const w = window.open()
-w.document.body.innerHTML = "<h1>You can add HTML something like this</h1>"
 
-const style = w.document.createElement("link")
-link.href = "/path/to.css"
-link.rel = "stylesheet"
-w.document.head.appendChild(style)
